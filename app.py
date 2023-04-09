a27b0cda223f05f49b4c6a4a808af25691d81cb7
Taylor Wagstaff <taylorwagstaff@gmail.com>
2023-04-09 21:44:07 +1200

    back to basic

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from flask import Flask, render_template
-import chrome_bookmarks
-import random
+
 
 # . venv/bin/activate
 
-bookmarks_url = []
+# bookmarks_url = []
 
 # for folder in chrome_bookmarks.folders:
 #     print(folder.name)
 #     print(folder.folders)
 
-for url in chrome_bookmarks.urls:
-    bookmarks_url.append(url.url)
+# for url in chrome_bookmarks.urls:
+#     bookmarks_url.append(url.url)
 
 
 # sets up the website for python
@@ -27,15 +26,15 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/run_code', methods=['POST'])
-def run_code():
-    result = "\n".join(map(str, random.choice(bookmarks_url)))
-    return result
+# @app.route('/run_code', methods=['POST'])
+# def run_code():
+#     result = "\n".join(map(str, random.choice(bookmarks_url)))
+#     return result
 
-@app.route('/run_all', methods=['POST'])
-def run_all():
-    result = "\n".join(map(str, bookmarks_url))
-    return result
+# @app.route('/run_all', methods=['POST'])
+# def run_all():
+#     result = "\n".join(map(str, bookmarks_url))
+#     return result
 
 # @app.route('/search', methods=['POST'])
 # def search():
